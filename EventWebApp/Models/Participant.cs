66ff81685e8c017d592270bd01fbef687d3ee203
case112 <oskar@nikopensius.ee>
2021-02-25 08:45:25 +0200

    Add initial migration

---
+++ EventWebApp/Models/Participant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventWebApp.Models
+{
+    public class Participant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
